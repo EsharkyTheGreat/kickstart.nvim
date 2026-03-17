5f379b53cc2f067b80779c4d452a88ec12fbdd5c
EsharkyTheGreat <eshwars1210@gmail.com>
2026-03-18 00:47:08 +0530

    adding typescript, ruff, pyright lsp

--- init.lua
+++ init.lua
@@ -613,7 +613,11 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
+
+        ruff = {},
+
+        pyright = {},
 
         stylua = {}, -- Used to format Lua code
 
